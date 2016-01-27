7d14250320d9c69354ee5d45e9bfb372a8d11299
unknown <turnern1@PSLW24629.b2b.regn.net>
2016-01-27 13:38:24 +0000

    automation agent

--- manifests/monitoring_agent.pp
+++ manifests/monitoring_agent.pp
@@ -5,42 +5,42 @@
 #
 class mongodb_ops_manager::monitoring_agent(
   $mmsApiKey   = '',
+  $mmsGroupId  = '',
   $version     = '2.5.15.1526-1',
   $platform    = 'rhel7',
   $mmsBaseUrl  = 'http://127.0.0.1:8080',
 )
 {
 
-  exec { 'download-mms-monitoring-agent':
+  exec { 'download-mms-automation-agent':
     command => "curl -OL ${mmsBaseUrl}/download/agent/automation/mongodb-mms-automation-agent-manager-${version}.x86_64.${platform}.rpm",
     cwd     => '/tmp',
     creates => "/tmp/mongodb-mms-automation-agent-manager-${version}.x86_64$.{platform}.rpm",
   }
 
-  exec { 'install-mms-monitoring-agent':
+  exec { 'install-mms-automation-agent':
     cwd     => '/tmp',
-    creates => '/usr/bin/mongodb-mms-monitoring-agent',
+    creates => '/run/mongodb-mms-automation',
     command => "rpm -U  \"/tmp/mongodb-mms-automation-agent-manager-${version}.x86_64.${platform}.rpm\"",
-    require => Exec['download-mms-monitoring-agent'],
+    require => Exec['download-mms-automation-agent'],
     timeout => 0
   }
   
- # file { '/etc/mongodb-mms/automation-agent.config':
- #   content => template('mongodb_ops_manager/automation-agent.config.erb'),
- #   owner   => 'mongodb-mms-agent',
- #   group   => 'mongodb-mms-agent',
- #   mode    => '0600',
- #   require => Exec['install-mms-monitoring-agent'],
- # }
+  file { '/etc/mongodb-mms/automation-agent.config':
+    content => template('mongodb_ops_manager/automation-agent.config.erb'),
+    owner   => 'mongod',
+    group   => 'mongod',
+    mode    => '0600',
+    require => Exec['install-mms-automation-agent'],
+  }
   
- # service { 'mongodb-mms-monitoring-agent':
- #   ensure    => running,
- #   enable    => true,
- #   hasstatus => true,
- #   restart   => true,
- #   provider  => 'init',
- #   require   => File['/etc/mongodb-mms/monitoring-agent.config']
- # }  
+  service { 'mongodb-mms-automation-agent':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    restart   => true,
+    require   => File['/etc/mongodb-mms/automation-agent.config']
+  }  
   
  
 }
