6b7d2fa857850bb2826808b36720437c3f9834e5
Jeff Engstrom <jeff.engstrom@nike.com>
2016-04-16 15:09:03 -0700

    adds binary search paths for rpm and curl

--- manifests/automation_agent.pp
+++ manifests/automation_agent.pp
@@ -22,6 +22,7 @@ class mongodb_ops_manager::automation_agent(
   exec { 'download-mms-automation-agent':
     command => "curl -OL ${mmsBaseUrl}/download/agent/automation/mongodb-mms-automation-agent-manager-${version}.x86_64${platform}.rpm",
     cwd     => '/tmp',
+    path    => '/usr/bin:$PATH',
     creates => "/tmp/mongodb-mms-automation-agent-manager-${version}.x86_64${platform}.rpm",
   }
 
@@ -29,9 +30,10 @@ class mongodb_ops_manager::automation_agent(
     ensure   => installed,
     source   => "/tmp/mongodb-mms-automation-agent-manager-${version}.x86_64${platform}.rpm",
     provider => 'rpm',
+    path     => '/bin:$PATH',
     require  => Exec['download-mms-automation-agent'],
   }
-  
+
   file { '/etc/mongodb-mms/automation-agent.config':
     content => template('mongodb_ops_manager/automation-agent.config.erb'),
     owner   => 'mongod',
@@ -39,7 +41,7 @@ class mongodb_ops_manager::automation_agent(
     mode    => '0600',
     require => Package['mongodb-mms-automation-agent-manager.x86_64'],
   }
-  
+
   service { 'mongodb-mms-automation-agent':
     ensure    => running,
     enable    => true,
@@ -47,5 +49,5 @@ class mongodb_ops_manager::automation_agent(
     restart   => true,
     require   => File['/etc/mongodb-mms/automation-agent.config']
   }
- 
+
 }
